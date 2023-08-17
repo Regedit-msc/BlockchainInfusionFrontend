b44249853112db42750b00f46563ec90173b38a3
mide <efusanyaae@gmail.com>
2023-08-17 10:14:34 +0100

    Initialized new project

---
+++ index.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector("h1");
+
+document.querySelector("#btn1").onclick = () => {
+  h1.style.color = "red";
+};
+
+document.querySelector("#btn2").onclick = () => {
+  h1.style.fontSize = "50px";
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Blockchain Infusion Frontend
+- [x] Simple HTML Page
+- [x] Introduction to Git (using Github as remote) and JavaScript.
